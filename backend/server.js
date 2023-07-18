91e8e0290a08096f94305f44acd65bb01fc8ca2d
ic3surf3r <novis.joao@gmail.com>
2023-07-18 18:39:45 +0200

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
-const port = 5500;
+const port = process.env.PORT || 5500;
 
 const app = express();
 
