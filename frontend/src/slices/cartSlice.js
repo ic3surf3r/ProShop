2d4000581832eaa98661a318595727843d1a6200
ic3surf3r <novis.joao@gmail.com>
2023-07-23 16:15:37 +0200

    add to cart function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,46 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) => {
+          x._id === existItem._id ? item : x;
+        });
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      // Calculate shipping price (If oreder is over €200 then free, else €20)
+      state.shippingPrice = addDecimals(state.itemsPrice > 200 ? 0 : 20);
+
+      // Calculate tax price (tax included in price cause europe is better)
+      //   state.taxPrice = 0;
+
+      // Calculate total price
+      state.totalPrice = Number(state.itemsPrice) + Number(state.shippingPrice);
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
