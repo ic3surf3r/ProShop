a30087806226bf210738057776845f28f05b22b6
ic3surf3r <novis.joao@gmail.com>
2023-07-26 14:49:57 +0200

    Auth & User API Slice

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,5 +2,6 @@
 //   process.env.NODE_ENV === "development" ? "http://localhost:5500" : "";
 export const BASE_URL = "";
 export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/user";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
