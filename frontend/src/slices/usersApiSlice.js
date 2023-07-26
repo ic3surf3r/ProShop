8093f624bad3a3a6c358e38abf35482a7d45cfef
Ic3surf3r <novis.joao@gmail.com>
2023-07-27 00:00:02 +0200

    login functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -5,8 +5,8 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: USERS_URL / login,
-        method: POST,
+        url: `${USERS_URL}/login`,
+        method: "POST",
         body: data,
       }),
     }),
