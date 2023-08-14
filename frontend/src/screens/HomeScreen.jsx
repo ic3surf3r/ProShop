41a6894e985a5376865f66d32f7c300583c2f89d
Ic3surf3r <novis.joao@gmail.com>
2023-08-14 07:04:28 +0200

    search products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,9 +7,12 @@ import Message from "../components/Message";
 import Paginate from "../components/Paginate";
 
 function HomeScreen() {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
 
   return (
     <>
@@ -32,7 +35,11 @@ function HomeScreen() {
               </Col>
             ))}
           </Row>
-          <Paginate pages={data.pages} page={data.page} />
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ""}
+          />
         </>
       )}
     </>
