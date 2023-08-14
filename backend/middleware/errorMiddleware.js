cb5e4fec55c3905687e2a192a69446b7427068d2
Ic3surf3r <novis.joao@gmail.com>
2023-08-14 22:50:02 +0200

    ready to deploy

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,11 +8,11 @@ const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  // Check for Mongoose bad ObsjectId
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = `Resource not found`;
-    statusCode = 404;
-  }
+  // // Check for Mongoose bad ObjectId
+  // if (err.name === "CastError" && err.kind === "ObjectId") {
+  //   message = `Resource not found`;
+  //   statusCode = 404;
+  // }
 
   res.status(statusCode).json({
     message,
