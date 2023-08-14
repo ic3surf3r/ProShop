6014baed41755a5b8367284209aeddada8924202
Ic3surf3r <novis.joao@gmail.com>
2023-08-14 07:17:25 +0200

    searchbox

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
@@ -16,6 +17,11 @@ function HomeScreen() {
 
   return (
     <>
+      {keyword && (
+        <Link to="/" className="btn btn-light mb-4">
+          GoBack
+        </Link>
+      )}
       {isLoading ? (
         <>
           <br />
@@ -27,7 +33,7 @@ function HomeScreen() {
         </Message>
       ) : (
         <>
-          <h1>Latest Products</h1>
+          {!keyword ? <h1>Latest Products</h1> : <h1>{keyword}</h1>}
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
